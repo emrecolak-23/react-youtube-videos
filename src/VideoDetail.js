04d9159e026d209beb27317a40adb9c5131a2cdf
Emre Çolak <emrecolak@Emre.home>
2023-01-28 20:22:17 +0300

    scaffolding components added

---
+++ src/VideoDetail.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class VideoDetail extends Component {
+  render() {
+    return <div>Video Detail</div>;
+  }
+}
+
+export default VideoDetail;
---
+++ src/VideoList.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class VideoList extends Component {
+  render() {
+    return <div>Video List</div>;
+  }
+}
+
+export default VideoList;
