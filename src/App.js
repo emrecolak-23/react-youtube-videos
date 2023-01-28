428aa149156b64e9cdd70861f64dd7099777e24e
Emre Çolak <emrecolak@Emre.home>
2023-01-28 21:04:33 +0300

    add event hadnlers and handling form submittal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import { Component } from 'react';
 
+import SearchBar from './SearchBar';
+
 class App extends Component {
+  state = { videos: [] };
+
+  onSubmitSearch = (term) => {
+    console.log(term);
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSubmitSearch} />
+      </div>
+    );
   }
 }
 
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,8 +1,34 @@
 import { Component } from 'react';
 
 class SearchBar extends Component {
+  //   constructor(props) {
+  //     super(props);
+
+  //     this.onChangeSearch = this.onChangeSearch.bind(this);
+  //   }
+
+  state = { term: '' };
+
+  onSubmitForm = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onSubmitForm} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(event) => this.setState({ term: event.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
